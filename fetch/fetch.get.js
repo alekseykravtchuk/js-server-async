45be11e13d148abb1893389b4fa99b3797dfd1d6
Aliaksei Krauchuk <47940682+alekseykravtchuk@users.noreply.github.com>
2020-01-31 16:07:42 +0300

    Add files via upload

---
+++ fetch/fetch.get.js
@@ -0,0 +1,19 @@
+const requestURL = 'https://jsonplaceholder.typicode.com/users';
+
+function sendRequest(method, url, body = null) {
+    return fetch(url).then(response => {
+        return response.json();
+    })
+}
+
+sendRequest('GET', requestURL)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+// const body = {
+//     name: 'Alexei',
+//     age: 27
+// };
+// sendRequest('POST', requestURL, body)
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
